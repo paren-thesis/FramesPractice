0b976c878bc16e96c5b9ace142c7c1e0dda14586
p@ren <ntimprosper308@gmail.com>
2025-06-12 19:19:00 +0000

    Add some methods

--- src/framespractice/PenssionChild.java
+++ src/framespractice/PenssionChild.java
@@ -5,10 +5,33 @@
  */
 package framespractice;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Prosper
  */
-public class PenssionChild {
+public class PenssionChild extends Penssion{
+    
+    private double year1, year2, year3;
+
+    public PenssionChild(double year1, double year2, double year3, LocalDate dob, int months) {
+        super(dob, months);
+        this.year1 = year1;
+        this.year2 = year2;
+        this.year3 = year3;
+    }
+    
+    public double avgSalary() {
+        return (year1 + year2 + year3) / 3;
+    }
+    
+    public double annualPension() {
+        return avgSalary() * (pRight()/100);
+    }
+    
+    public double monthlyPenssion() {
+        return annualPension() / 12;
+    }
     
 }
