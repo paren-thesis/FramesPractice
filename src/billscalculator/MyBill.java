0130f260bff7f8c7a5b1bf92e5f7e2dd85699d1a
p@ren <ntimprosper308@gmail.com>
2025-08-16 11:16:29 +0000

    Complete MyBill class

--- src/billscalculator/MyBill.java
+++ src/billscalculator/MyBill.java
@@ -9,6 +9,33 @@ package billscalculator;
  *
  * @author Prosper
  */
-public class MyBill {
+public class MyBill extends Bills {
+
+    public MyBill() {
+        super(1, 1, 1);
+    }
+
+    public MyBill(double gallons, double minutes, double kilowatts) {
+        super(gallons, minutes, kilowatts);
+    }
+
+    public double electricityBill() {
+        if (getKilowatts() <= 500) {
+            return 0;
+        } else {
+            return (getKilowatts() - 500) * 2.01;
+        }
+    }
+
+    public double telephoneBill() {
+        if (getMinutes() <= 5) {
+            return 0;
+        } else {
+            return (getMinutes() - 5) * 0.60;
+        }
+    }
     
+    public double totalBill() {
+        return waterBill() + telephoneBill() + electricityBill();
+    }
 }
