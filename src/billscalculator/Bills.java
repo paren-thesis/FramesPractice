b52e528db8d5105c153d5dc4ffb7f01d357cce72
p@ren <ntimprosper308@gmail.com>
2025-08-16 11:09:17 +0000

    Complete Bill class

--- src/billscalculator/Bills.java
+++ src/billscalculator/Bills.java
@@ -10,5 +10,32 @@ package billscalculator;
  * @author Prosper
  */
 public class Bills {
-    
+
+    private double gallons, minutes, kilowatts;
+
+    public Bills(double gallons, double minutes, double kilowatts) {
+        this.gallons = gallons;
+        this.minutes = minutes;
+        this.kilowatts = kilowatts;
+    }
+
+    public double getGallons() {
+        return gallons;
+    }
+
+    public double getMinutes() {
+        return minutes;
+    }
+
+    public double getKilowatts() {
+        return kilowatts;
+    }
+
+    public double waterBill() {
+        if (gallons <= 1000) {
+            return 0;
+        } else {
+            return (gallons - 1000) - 0.05;
+        }
+    }
 }
